878296233cf0c45876901d53d98d3cd9f7da6d97
SriramBalasubramaniyan <srirambalasubramaniyan.b@gmail.com>
2026-03-28 08:56:17 +0530

    top_k filter score updated for better context extraction

--- src/embedding/search/semantic-search.py
+++ src/embedding/search/semantic-search.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 import numpy as np
 import pickle
 from pathlib import Path
-from google.genai import types
-from time import sleep
 
 load_dotenv()
 
@@ -18,24 +16,9 @@ max_history_length = int(os.getenv("Max_History_Length").strip() or 6)
 cache_file = "embeddings.pkl"
 cache_dir = Path.cwd()/"data"
 
-gen_model_name = ""
 embed_model_name = ""
 
 cache_file_path = cache_dir/cache_file
-
-system_prompt = """
-    Rules:
-    - Answer in friednly and easy to understand terms
-    - dont ask follow up questions
-    - Explain step by step
-    - Do not use outside knowledge
-    - Do not hallucinate
-    - If you don't know the answer, say "I don't know" instead of making up an answer.
-    - Never share any personal information or sensitive data.
-    """
-
-if gen_model_name == "":
-    gen_model_name = os.getenv("GEN_MODEL_NAME").strip() or client.models.list()[0].name
 
 documents = [
     "Flutter is used for mobile development",
@@ -46,27 +29,6 @@ documents = [
 
 if embed_model_name == "":
     embed_model_name = os.getenv("EMBEDDING_MODEL_NAME").strip() or "gemini-embedding-001"
-
-history = []
-
-def estimate_tokens(text):
-    return int(len(text) / 4)  # Rough estimate: 1 token ≈ 4 characters
-
-def count_history_tokens(history, system_prompt):
-    total_tokens = estimate_tokens(system_prompt)
-    for entry in history:
-        for part in entry["parts"]:
-            total_tokens += estimate_tokens(part["text"])
-    return total_tokens
-
-def trim_history(history, system_prompt):
-    while count_history_tokens(history, system_prompt) > max_tokens:
-        if len(history) >= 2:
-            history.pop(0) # Remove the oldest User entry
-            history.pop(0) # Remove the corresponding Model entry
-        else:
-            break
-    return history
 
 try:
     cache_dir.mkdir(parents=True,exist_ok=True)
@@ -115,7 +77,7 @@ while True:
             score = cosine_similarity(qry_emb, emb)
             scores.append((doc, score))
         
-        top_k = [(doc, score) for doc, score in scores if score > 0.5][:3]
+        top_k = [(doc, score) for doc, score in scores if score > 0.6][:3]
 
         if not top_k:
             print("No relevant context found")
@@ -123,53 +85,7 @@ while True:
 
         context = "\n".join([doc for doc, _ in top_k])
 
-        bot_reply = ""
-
-        prompt = f'''
-            answer the question only using the context below
-            context:{context}
-            question:{qry}
-        '''
-
-        # Trim history BEFORE sending    
-        history = trim_history(history, system_prompt)
-
-        stream = client.models.generate_content_stream(
-            model=gen_model_name,
-            contents=history + [{
-                "role": "user",
-                "parts": [{"text": prompt}]
-            }], # Pass the conversation history to maintain context
-            config=types.GenerateContentConfig(
-                temperature=0.7, # Adjust the temperature for more creative responses
-                system_instruction=[{"text": system_prompt}]
-                # Provide the system prompt as a list to ensure it's included in the generation process
-            ),
-        )
-
-        print(f"{gen_model_name}: ", end="", flush=True)
-
-        for chunk in stream:
-            if chunk.text:
-                for line in chunk.text.splitlines():
-                    for char in line:
-                        print(char, end="", flush=True)
-                        sleep(0.01)
-                        bot_reply += char
-                    print()  # Move to the next line after the bot finishes replying
-                    bot_reply += "\n"  # Add a newline after each line of the bot's response
-
-
+        print(f"Best Match:\n{context}")
     except Exception as e:
         print(f"An error occurred: {e}")
         continue
-
-    history.append({
-        "role": "user",
-        "parts": [{"text": qry}]
-    })
-
-    history.append({
-        "role": "model",
-        "parts": [{"text": bot_reply}]
-    })
